30406d83312dbc5aae291fd2530e6595b6bc590f
Artem Zamuruev <artem.zamuruev@gmail.com>
2016-08-21 20:56:35 +0000

    Легкий рефакторинг кода, существенных изменений нет

--- js/easy_parallax.js
+++ js/easy_parallax.js
@@ -15,39 +15,31 @@ var currentCursorCords = {
 }
 
 $(document).on("mousemove", function(event){
-    
     if (prevCursorCords.x == 0 && prevCursorCords.y == 0){
         prevCursorCords.x = event.clientX;
         prevCursorCords.y = event.clientY;
     }
-    
     currentCursorCords.x = event.clientX;
     currentCursorCords.y = event.clientY;
-    
     var shiftTop = parseFloat((currentCursorCords.y - prevCursorCords.y)/-75);
     var shiftLeft = parseFloat((currentCursorCords.x - prevCursorCords.x)/-100);
-    
     if(Math.abs(shiftTop) > 5 || Math.abs(shiftLeft) > 5){
         return;
     }
-    
     var $icons = $("div.shedule__icon-back");
     var $ticket_icons = $("div.tickets__icon");
-    
     $icons.each(function(){
         $(this).css({
             "top": parseFloat($(this).css("top")) + shiftTop,
             "left": parseFloat($(this).css("left")) + shiftLeft
         });
     });
-    
     $ticket_icons.each(function(){
         $(this).css({
             "top": parseFloat($(this).css("top")) + shiftTop,
             "left": parseFloat($(this).css("left")) + shiftLeft
         });
     });
-    
     prevCursorCords.x = event.clientX;
     prevCursorCords.y = event.clientY;
     
