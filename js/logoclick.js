37e65e121a75e76df2819cb0c01209a78bfa387c
Artem Zamuruev <artem.zamuruev@gmail.com>
2016-08-21 21:15:56 +0000

    Теперь при нажатии на лого происходит скролл наверх, а не перезагрузка страницы

---
+++ js/logoclick.js
@@ -0,0 +1,13 @@
+/**
+* Created with FutureByShmakov.
+* User: ArtemZamuruev
+* Date: 2016-08-21
+* Time: 09:08 PM
+* To change this template use Tools | Templates.
+*/
+
+$("div.header__logo").click(function(){
+    $("body").animate({
+        scrollTop : 0
+    }, 500);
+});
